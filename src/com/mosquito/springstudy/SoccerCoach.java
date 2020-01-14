e47c0c9ebd5393440b9b8f9e6806e0c57b28a73e
Giovane de Oliveira <giovane.oliveira.13@outlook.com>
2020-01-14 13:02:55 -0300

    7_configuration-with-code-no-xml

--- src/com/mosquito/springstudy/SoccerCoach.java
+++ src/com/mosquito/springstudy/SoccerCoach.java
@@ -1,28 +1,31 @@
 package com.mosquito.springstudy;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Scope;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
-@Scope("prototype")
 public class SoccerCoach implements Coach {
 
 	private FortuneService fortuneService;
 
-	@Autowired
-	public void setWithAnyMethodName(FortuneService fortuneService) {
+	@Value("${team}")
+	private String team;
+	
+	@Value("${player}")
+	private String player;
+	
+	public SoccerCoach(FortuneService fortuneService) {
 		this.fortuneService = fortuneService;
 	}
 
 	@Override
 	public String getDailyWorkout() {
-		return "Penalty kick for 30 minutes";
+		return "Team: " + team + ", Player: " + player + ", Drill: Penalty kick for 30 minutes";
 	}
 
 	@Override
 	public String getDailyFortune() {
 		return fortuneService.getFortune();
 	}
-
+	
 }
--- src/com/mosquito/springstudy/SpringApp.java
+++ src/com/mosquito/springstudy/SpringApp.java
@@ -1,32 +1,16 @@
 package com.mosquito.springstudy;
 
-import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class SpringApp {
 
 	public static void main(String[] args) {
-		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+		AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(SportConfig.class);
 		
-		Coach coach1 = context.getBean("hockeyCoach", Coach.class);
-		Coach coach2 = context.getBean("hockeyCoach", Coach.class);
+		Coach coach = context.getBean("soccerCoach", Coach.class);
 		
-		System.out.println("\n== Singleton Scope ==");
-		System.out.println(coach1.getDailyWorkout());
-		System.out.println(coach1.getDailyFortune());
-		System.out.println("Coach 1: " + coach1);
-		System.out.println("Coach 2: " + coach2);
-		System.out.println("Are equal: " + (coach1 == coach2));
-		
-
-		coach1 = context.getBean("soccerCoach", Coach.class);
-		coach2 = context.getBean("soccerCoach", Coach.class);
-		
-		System.out.println("\n== Prototype Scope ==");
-		System.out.println(coach1.getDailyWorkout());
-		System.out.println(coach1.getDailyFortune());
-		System.out.println("Coach 1: " + coach1);
-		System.out.println("Coach 2: " + coach2);
-		System.out.println("Are equal: " + (coach1 == coach2));
+		System.out.println(coach.getDailyWorkout());
+		System.out.println(coach.getDailyFortune());
 		
 		context.close();
 	}
