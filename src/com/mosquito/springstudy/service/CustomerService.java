05d5e3761c600da9421b4b54a57b49b44db13f2c
Giovane de Oliveira <giovane.oliveira.13@outlook.com>
2020-01-25 17:19:36 -0300

    30_db-web-app-spring-mvc-hibernate-3

---
+++ src/com/mosquito/springstudy/service/CustomerService.java
@@ -0,0 +1,11 @@
+package com.mosquito.springstudy.service;
+
+import java.util.List;
+
+import com.mosquito.springstudy.entity.Customer;
+
+public interface CustomerService {
+
+	public List<Customer> getCustomers();
+	
+}
