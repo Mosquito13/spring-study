87747be67f5a42260a56f8b948b237a81d4153d2
Giovane de Oliveira <giovane.oliveira.13@outlook.com>
2020-01-26 16:06:32 -0300

    38_aop-pointcut-declarations

--- src/com/mosquito/springstudy/aspect/LoggingAspect.java
+++ src/com/mosquito/springstudy/aspect/LoggingAspect.java
@@ -2,15 +2,33 @@ package com.mosquito.springstudy.aspect;
 
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
 
 @Aspect
 @Component
 public class LoggingAspect {
 	
-	@Before("execution(* add*())")
-	public void beforeAddAccountAdvice() {
+	@Pointcut("execution(* com.mosquito.springstudy.dao.*.*(..))")
+	private void allDaoClassMethods() {}
+	
+	@Pointcut("execution(* com.mosquito.springstudy.dao.*.set*(..))")
+	private void allDaoClassSetter() {}
+	
+	@Pointcut("execution(* com.mosquito.springstudy.dao.*.get*(..))")
+	private void allDaoClassGetter() {}
+	
+	@Pointcut("allDaoClassMethods() && !allDaoClassSetter() && !allDaoClassGetter()")
+	private void allDaoClassNoSetterGetterMethod() {}
+	
+	@Before("allDaoClassNoSetterGetterMethod()")
+	public void beforeDaoMethods() {
 		System.out.println("before - advice");
+	}
+	
+	@Before("allDaoClassNoSetterGetterMethod()")
+	public void beforeDaoMethodsAnotherOne() {
+		System.out.println("i'm another");
 	}
 	
 }
