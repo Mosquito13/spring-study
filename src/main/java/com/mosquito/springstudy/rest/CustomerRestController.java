06e58bbf2919a12c90bf85770374c0bd546fe2d0
Giovane de Oliveira <giovane.oliveira.13@outlook.com>
2020-01-28 23:09:41 -0300

    64_spring-rest-crud-2

---
+++ src/main/java/com/mosquito/springstudy/rest/CustomerRestController.java
@@ -0,0 +1,30 @@
+package com.mosquito.springstudy.rest;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.mosquito.springstudy.entity.Customer;
+import com.mosquito.springstudy.service.CustomerService;
+
+@RestController
+@RequestMapping("/api")
+public class CustomerRestController {
+
+	@Autowired
+	private CustomerService customerService;
+	
+	@GetMapping("/customers")
+	public List<Customer> getCustomers() {
+		return customerService.getCustomers();
+	}
+	
+	@GetMapping("/customers/{customerId}")
+	public Customer getCustomers(@PathVariable int customerId) {
+		return customerService.getCustomer(customerId);
+	}
+}
