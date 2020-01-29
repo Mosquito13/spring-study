78570a22a49f27e46fba95aaf9c17bbe627bc088
Giovane de Oliveira <giovane.oliveira.13@outlook.com>
2020-01-28 23:37:56 -0300

    66_spring-rest-crud-4

--- src/main/java/com/mosquito/springstudy/rest/CustomerRestController.java
+++ src/main/java/com/mosquito/springstudy/rest/CustomerRestController.java
@@ -5,6 +5,8 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -32,5 +34,14 @@ public class CustomerRestController {
 		}
 		
 		return c;
+	}
+	
+	@PostMapping("/customers")
+	public Customer postCustomer(@RequestBody Customer customer) {
+		customer.setId(0);
+		
+		customerService.saveCustomer(customer);
+		
+		return customer;
 	}
 }
